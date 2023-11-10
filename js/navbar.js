d3e2350602e650c5a0b05af9d538b98dac48bc38
Bruna Moura <104026303+nOpfiNe@users.noreply.github.com>
2023-11-10 17:24:36 -0300

    up

--- js/navbar.js
+++ js/navbar.js
@@ -3,9 +3,17 @@ function toggleNav() {
     nav.classList.toggle('nav-open');
     
     const icon = document.querySelector("#icon");
-    icon.classList.toggle('bi-list'); // Adicione a classe para o ícone de barras
-    icon.classList.toggle('bi-x-lg'); // Remova a classe para o ícone de vezes
+    icon.classList.toggle('bi-list');
+    icon.classList.toggle('bi-x-lg');
+
+    const clipPath = document.querySelector("#clip-path");
+    clipPath.style.display = (nav.classList.contains('nav-open')) ? 'block' : 'none';
 }
 
 const navToggle = document.querySelector("#navToggle");
 navToggle.addEventListener("click", toggleNav);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clipPath = document.querySelector("#clip-path");
+    clipPath.style.display = 'none';
+});
